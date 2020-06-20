33687da14583598df26b18d310a93d9c89be01cf
Bruno Franki <bruno.franki@gmail.com>
2020-06-20 18:48:00 +0200

    -am

--- index.js
+++ index.js
@@ -1,74 +1,23 @@
-const fs = require('fs');
-const path = require('path');
 const core = require('@actions/core');
-const github = require('@actions/github');
+const wait = require('./wait');
+const AWS = require('aws-sdk');
+const ApiGtw = require('./lib/api-gtw')
 
+// most @actions toolkit packages have async methods
+async function run() {
+  try { 
+       
+    const swaggerPath = core.getInput('swagger-path') || `${process.cwd()}/swagger/swagger.json`;
+    const swaggerExtPath = core.getInput('swagger-with-extension-path') || `${process.cwd()}/swagger/swagger_ext.json`;
+    const tmpPath = core.getInput('tmp-path') || '/tmp';
+    const apiName = core.getInput('api-name') || 'Party Capability API';
+    const region = core.getInput('aws-region') || 'eu-west-1';
+    AWS.config.update({ region }); 
 
-const swaggerExtJson = JSON.parse(fs.readFileSync(`${process.cwd()}/swagger/swagger_ext.json`));
-
-try {
-  const swaggerPath = core.getInput('swagger-path');
-  const swaggerJson = JSON.parse(fs.readFileSync(swaggerPath));
-
-  const swaggerExtPath = core.getInput('swagger-with-extension-path');
-  const swaggerExtJson = JSON.parse(fs.readFileSync(swaggerExtPath));
-
-  const tmpPath = core.getInput('tmp-path');
-
-  console.log("============ SWAGGER (JAVA) ====================================================");
-  console.log("BEFORE", JSON.stringify(swaggerJson));
-  for(var [key, value, jsonpath] of traverse(swaggerExtJson)) {
-    if (key.startsWith('x-amazon')) {
-      // We found an extension. Now find the parent object that owns this extension
-      const parent = jsonpath.slice(0, -1);
-
-      // We know the parent object and therefore its path in the Swagger object
-      // Let's find if this parent object also exists in the Swagger file from JAVA
-      // If it's the case, then add the extension to it
-      const originalObj = get(swaggerJson, parent);
-      if (originalObj && !originalObj[key]) {
-        originalObj[key] = value;
-      }
-    }
+  } 
+  catch (error) {
+    core.setFailed(error.message);
   }
-  console.log("============ SWAGGER (MERGED) ====================================================");
-  const mergedJson = JSON.stringify(swaggerJson);
-  console.log("MERGED", mergedJson);
-
-  // Save the merged JSON in a file
-  const swaggerMergedPath = path.join(tmpPath, 'merged-swagger.json');
-  fs.writeFileSync(swaggerMergedPath, mergedJson);
-  core.setOutput("merged-swagger-path", swaggerMergedPath);
-} catch (error) {
-  core.setFailed(error.message);
 }
 
-function* traverse(o) {
-  const memory = new Set();
-  function * innerTraversal (o, jsonpath=[]) {
-    if(memory.has(o)) {
-      // we've seen this object before don't iterate it
-      return;
-    }
-    // add the new object to our memory.
-    memory.add(o);
-    for (var i of Object.keys(o)) {
-      const itemPath = jsonpath.concat(i);
-      yield [i,o[i],itemPath]; 
-      if (o[i] !== null && typeof(o[i])=="object") {
-        //going one step down in the object tree!!
-        yield* innerTraversal(o[i], itemPath);
-      }
-    }
-  }
-    
-  yield* innerTraversal(o);
-}
-
-function get(obj, keys) {
-  for (let i = 0; i < keys.length - 1; i++) {
-    obj = obj[keys[i]];
-    if (!obj) return null;
-  }
-  return obj;
-}
+run()
