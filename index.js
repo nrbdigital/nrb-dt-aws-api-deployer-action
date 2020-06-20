61a7e292a619f57f1b5204db4d0ca75c27006ca5
Bruno Franki <bruno.franki@gmail.com>
2020-06-20 20:07:20 +0200

    exportApi

--- index.js
+++ index.js
@@ -10,10 +10,22 @@ async function run() {
     const swaggerPath = core.getInput('swagger-path') || `${process.cwd()}/swagger/swagger.json`;
     const swaggerExtPath = core.getInput('swagger-with-extension-path') || `${process.cwd()}/swagger/swagger_ext.json`;
     const tmpPath = core.getInput('tmp-path') || '/tmp';
-    const apiName = core.getInput('api-name') || 'Party Capability API';
+    const apiName = core.getInput('api-name') || 'My First API';
     const region = core.getInput('aws-region') || 'eu-west-1';
     AWS.config.update({ region }); 
 
+    const apiGtw = new ApiGtw();
+    let awsApi = await apiGtw.getApiByName(apiName);
+    if (!awsApi) {
+      // TODO: create the API
+      console.log("must create the API");
+      awsApi = await apiGtw.createApi(apiName, 'TODO Description');
+      console.log("New API", awsApi);
+    } else {
+      console.log("Existing API", awsApi);
+      const awsSwagger = await apiGtw.exportApi(awsApi.id)
+      console.log(awsSwagger.body.toString());
+    }
   } 
   catch (error) {
     core.setFailed(error.message);
