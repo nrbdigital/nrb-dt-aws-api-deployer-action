9b0ecf229cad054feee6edda53f146453d30b68d
michael-stappers <58463286+michael-stappers@users.noreply.github.com>
2021-01-18 16:30:26 +0100

    bug fix + clean

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ async function run() {
   try { 
     const targetEnv = core.getInput('target-env') || 'dev';
     const swaggerPath = core.getInput('swagger-path') || `${process.cwd()}/swagger.json`;
-    const apiName = core.getInput('api-name') || 'test1-experience-api-v1';
+    const apiName = core.getInput('api-name') || 'Test githubaction API v1';
     const region = core.getInput('aws-region') || 'eu-west-1';
     const basePath = core.getInput('api-base-path') || 'test1-exp-v1';
     const domainName = core.getInput('api-domain-name') || 'dev.openapi.ethias.be';
@@ -33,16 +33,21 @@ async function run() {
     console.log("================== Imported API", JSON.stringify(importedApi, null, 2));
     
     // Deploy the API on default stage
-    const deployedApi = await apiGtw.createDeployment(importedApi.id, "CICD deployment", "default", "Default");
+    const deployedApi = await apiGtw.createDeployment(importedApi.id, "Test githubaction", "default", "Default");
     console.log("================== Deployed API", JSON.stringify(deployedApi, null, 2));
 
-    // associate API to custom domain name + base path
-    // and update stage (only for the first deployment)
+    // Associate a web ACL to the stage
+    let webAcl = await apiGtw.updateWebAcl(importedApi.id, domainName);
+    console.log("================== Web ACL", JSON.stringify(webAcl, null, 2));
+
+    // Add stage variable and enable logs
+    let stage = await apiGtw.updateStage(importedApi.id, domainName);
+    console.log("================== Stage", JSON.stringify(stage, null, 2));
+
+    // Associate API to custom domain name + base path
     let basePathMapping = await apiGtw.getBasePathMapping(basePath, domainName);
     if (!basePathMapping) {
       basePathMapping = await apiGtw.createBasePathMapping(importedApi.id, basePath, domainName);
-      webAcl = await apiGtw.updateWebAcl(importedApi.id, domainName);
-      stage = await apiGtw.updateStage(importedApi.id, domainName);
     }
 
     console.log("================== basePathMapping", JSON.stringify(basePathMapping, null, 2));
